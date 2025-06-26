c22435c5036e167a57dc1d66344f64f1fd45af72
MauriceBecker <maurice-becker@outlook.de>
2025-06-26 22:32:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      input: resolve(__dirname, 'public/index.html')
+    }
   }
 });
